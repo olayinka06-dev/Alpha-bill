55ef1092339fee6878eaba2c369f326b500dfc1a
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-03-22 05:22:38 -0700

    first commit

---
+++ src/dashboard/SideNav.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'
+import {TbLayoutDashboard} from 'react-icons/tb'
+import {CgData} from 'react-icons/cg'
+import {AiOutlineDatabase} from 'react-icons/ai'
+import {RiBilibiliLine} from 'react-icons/ri'
+import {TbWallet} from 'react-icons/tb'
+import {CgProfile} from 'react-icons/cg'
+import {IoSettingsOutline} from 'react-icons/io'
+import {BiLogIn} from 'react-icons/bi'
+
+const SideNav = () => {
+  return (
+    <Wrapper>
+        <nav>
+            <div className="top-side">
+
+            </div>
+            <div className="middle-side">
+
+            </div>
+            <div className="bottom-side">
+
+            </div>
+        </nav>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.nav`
+    
+
+
+`
+
+export default SideNav;
